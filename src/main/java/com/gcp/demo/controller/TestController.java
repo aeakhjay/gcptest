7a8002c4c88451b840066d70e7d7ebcfeede63cf
aeakhjay <46776862+aeakhjay@users.noreply.github.com>
2020-11-26 12:01:57 +0400

    comment

--- src/main/java/com/gcp/demo/controller/TestController.java
+++ src/main/java/com/gcp/demo/controller/TestController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/v1")
 public class TestController {
 
+	//
 	@GetMapping("/test")
 	public String test() {
 		return "GCP test success";
